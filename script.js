703e01aa50babf109878ccb9e4265834bb053330
Davi Freitas <df828316@gmail.com>
2021-12-09 18:12:34 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const elementTitle = document.getElementById('title');
 const paletaCores = document.getElementById('color-palette');
 
-const arrayColors = ['black', 'red', 'green', 'blue', 'yellow'];
+const arrayColors = ['black', 'red', 'green', 'blue'];
 for(let index in arrayColors) {
-    const colors = document.createElement('div');
+    let colors = document.createElement('div');
     colors.className = 'color';
+    colors.id = arrayColors[index];
     colors.style.backgroundColor = arrayColors[index];
     paletaCores.appendChild(colors);
 } 
+
+const black = document.getElementById('black');
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
 
 const quadroPixels = document.getElementById('pixel-board');
 for(let index = 0; index < 25; index += 1) {
@@ -16,3 +22,16 @@ for(let index = 0; index < 25; index += 1) {
     emBranco.style.backgroundColor = 'white';
     quadroPixels.appendChild(emBranco);
 }
+
+black.classList.add('selected');
+function selecionar(event) {
+    const selecionado = document.querySelector('.selected');
+    selecionado.classList.remove('selected');
+    event.target.classList.add('selected');
+    input.value = ''; 
+}
+
+black.addEventListener('click', selecionar);
+red.addEventListener('click', selecionar);
+green.addEventListener('click', selecionar);
+blue.addEventListener('click', selecionar);
