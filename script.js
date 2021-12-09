97f7b1f286e04037b249329627e3b7ea34283430
Davi Freitas <df828316@gmail.com>
2021-12-09 17:38:56 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const elementTitle = document.getElementById('title');
-const paletaDeCores = document.getElementById('color-palette');
+const paletaCores = document.getElementById('color-palette');
 
-const arrayColors = ['black', 'red', 'green', 'blue'];
+const arrayColors = ['black', 'red', 'green', 'blue', 'yellow'];
 for(let index in arrayColors) {
     const colors = document.createElement('div');
     colors.className = 'color';
     colors.style.backgroundColor = arrayColors[index];
-    paletaDeCores.appendChild(colors);
+    paletaCores.appendChild(colors);
 } 
+
+const quadroPixels = document.getElementById('pixel-board');
+for(let index = 0; index < 25; index += 1) {
+    const emBranco = document.createElement('div');
+    emBranco.className = 'pixel';
+    emBranco.style.backgroundColor = 'white';
+    quadroPixels.appendChild(emBranco);
+}
